93d95fa4bad61d9043a68e192c7530f34ec68389
mushano <mushano.takuya.sw@alumni.tsukuba.ac.jp>
2020-05-26 18:54:34 +0900

    initial commmit

---
+++ simple.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.cm as cm
+from matplotlib.ticker import ScalarFormatter
+from matplotlib import gridspec
+import os
+import csv
